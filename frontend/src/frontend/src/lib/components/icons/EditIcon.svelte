326b33961e4ce60031aff0cb2fc5a112a859e99e
jakepeg <jakepeg@gmail.com>
2025-03-13 10:36:22 +0000

    remove bg btn-ghost

--- frontend/src/frontend/src/lib/components/icons/EditIcon.svelte
+++ frontend/src/frontend/src/lib/components/icons/EditIcon.svelte
@@ -11,7 +11,7 @@
       y1="6.34614"
       x2="4.36472"
       y2="14.0492"
-      stroke="#0B8CE9"
+      stroke="currentColor"
       stroke-width="4"
     />
     <line
@@ -19,17 +19,17 @@
       y1="2.49458"
       x2="11.3791"
       y2="5.57582"
-      stroke="#0B8CE9"
+      stroke="currentColor"
       stroke-width="4"
     />
     <path
       d="M1.76844 17.0981L2.15135 13.5311L5.19258 16.0277L1.76844 17.0981Z"
-      fill="#0B8CE9"
+      fill="currentColor"
     />
   </g>
   <defs>
     <clipPath id="clip0_249_2">
-      <rect width="18" height="18" fill="white" />
+      <rect width="18" height="18" fill="currentColor" />
     </clipPath>
   </defs>
 </svg>
