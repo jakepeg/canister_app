887cb9387e0ea14939cf627fa0afb4be60ce3920
jakepeg <jakepeg@gmail.com>
2025-03-18 11:20:22 +0000

    no layout if not auth

--- frontend/src/frontend/src/lib/components/Home/NotAuthenticated.svelte
+++ frontend/src/frontend/src/lib/components/Home/NotAuthenticated.svelte
@@ -1,10 +1,18 @@
 <script>
   import PlaceholderLogo from "../icons/PlaceholderLogo.svelte";
+  import { authService } from "$lib/services/auth";
+  import LogoIcon from "../icons/LogoIcon.svelte";
 </script>
 
-<!-- <h1 class="title-1">My Files</h1> -->
-
-<div class=" pt-10 pb-4 text-center flex flex-col items-center gap-4 mt-6">
+<div class="pt-10 pb-4 text-center flex flex-col items-center gap-4 mt-6">
   <PlaceholderLogo />
-  <h2 class="">Please log in</h2>
+  <h2>Please log in</h2>
+
+  <button
+    class="gap-4 btn btn-accent flex items-center"
+    on:click={() => authService.login()}
+  >
+    <LogoIcon />
+    Login
+  </button>
 </div>
--- frontend/src/frontend/src/routes/+layout.svelte
+++ frontend/src/frontend/src/routes/+layout.svelte
@@ -7,69 +7,43 @@
   import { userStore } from "$lib/services/user";
   import { onMount } from "svelte";
   import "../app.css";
-  // import Disclaimer from "$lib/components/Disclaimer.svelte";
   import { ModeWatcher } from "mode-watcher";
   import * as Sidebar from "$lib/components/ui/sidebar";
-
-  const title = "Canister – Encrypted document sharing and requesting";
-  const description =
-    "Effortless document sharing on the Internet Computer. No plugins, no passwords. Canister enables seamless document requests, streamlining interactions for service providers and clients.";
-  const image = `https://${
-    import.meta.env.VITE_FRONTEND_CANISTER_ID
-  }.icp0.io/share.jpg`;
-  const url = `https://${
-    import.meta.env.VITE_FRONTEND_CANISTER_ID
-  }.icp0.io{$page.url.pathname}`;
-  const domain = `${import.meta.env.VITE_FRONTEND_CANISTER_ID}.icp0.io`;
 
   onMount(async () => {
     authService.init();
   });
 
-  const unregistered = $derived(
-    $authStore.state === "authenticated" && $userStore.state === "unregistered",
-  );
-
   let { children } = $props();
 </script>
 
-<svelte:head>
-  <title>{title}</title>
-  <meta name="description" content={description} />
-
-  <meta property="og:title" content={title} />
-  <meta property="og:description" content={description} />
-  <meta property="og:type" content="website" />
-  <meta name="og:image" content={image} />
-  <meta property="og:url" content={url} />
-
-  <meta name="twitter:card" content="summary_large_image" />
-  <meta name="twitter:site" content="@dfinity" />
-  <meta name="twitter:title" content={title} />
-  <meta name="twitter:description" content={description} />
-  <meta name="twitter:image" content={image} />
-  <meta property="twitter:url" content={url} />
-  <meta property="twitter:domain" content={domain} />
-</svelte:head>
-
-<div class="flex flex-col h-screen overflow-hidden gap-y-2">
-  <ModeWatcher />
-  <Navbar />
-  <div class="flex flex-1 overflow-hidden">
-    <Sidebar.Provider>
-      <AppSidebar />
-      <Sidebar.Inset class="flex-1 flex overflow-auto">
-        <main class="flex-1">
-          <Sidebar.Trigger class="sticky top-4 left-4" />
-          <div class="max-w-5xl mx-auto px-4 ml-10">
-            {@render children?.()}
-          </div>
-        </main>
-      </Sidebar.Inset>
-    </Sidebar.Provider>
+{#if $authStore.state === "authenticated"}
+  <!-- Authenticated layout -->
+  <div class="flex flex-col h-screen overflow-hidden gap-y-2">
+    <ModeWatcher />
+    <Navbar />
+    <div class="flex flex-1 overflow-hidden">
+      <Sidebar.Provider>
+        <AppSidebar />
+        <Sidebar.Inset class="flex-1 flex overflow-auto">
+          <main class="flex-1">
+            <Sidebar.Trigger class="sticky top-4 left-4" />
+            <div class="max-w-5xl mx-auto px-4 ml-10">
+              {@render children?.()}
+            </div>
+          </main>
+        </Sidebar.Inset>
+      </Sidebar.Provider>
+    </div>
   </div>
-</div>
 
-{#if $authStore.state === "authenticated" && $userStore.state === "unregistered"}
-  <RegistrationModal isOpen={unregistered} authenticatedStore={$authStore} />
+  {#if $authStore.state === "authenticated" && $userStore.state === "unregistered"}
+    <RegistrationModal
+      isOpen={$userStore.state === "unregistered"}
+      authenticatedStore={$authStore}
+    />
+  {/if}
+{:else}
+  <!-- No layout applied for non-authenticated users -->
+  {@render children?.()}
 {/if}
