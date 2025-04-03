25fbd3ca6065c53a68c6de40e781d461cfcb9c98
jakepeg <jakepeg@gmail.com>
2025-04-03 17:13:01 +0100

    add supported files (mp3/4 svg etc)

--- frontend/src/frontend/src/lib/components/FilePreview.svelte
+++ frontend/src/frontend/src/lib/components/FilePreview.svelte
@@ -9,7 +9,20 @@
     "image/jpeg",
     "image/png",
     "image/webp",
+    "image/gif",
+    "image/svg+xml",
+    "image/tiff",
+    "video/mp4",
+    "video/webm",
+    "video/ogg",
+    "audio/mpeg",
+    "audio/ogg",
+    "audio/wav",
+    "text/plain",
+    "text/csv",
+    "text/html",
   ];
+
   $: previewStyle =
     file.dataType === "application/pdf" ? "--bs-aspect-ratio: 141%" : "";
   $: previewClass = file.dataType === "application/pdf" ? "" : "";
@@ -25,8 +38,20 @@
         type="application/pdf"
         data={file.objectUrl}
         class="w-full aspect-square"
-      >
-      </object>
+      ></object>
+    {:else if file.dataType.startsWith("video/")}
+      <video controls class="w-full">
+        <source src={file.objectUrl} type={file.dataType} />
+        Your browser does not support the video tag.
+      </video>
+    {:else if file.dataType.startsWith("audio/")}
+      <audio controls class="w-full">
+        <source src={file.objectUrl} type={file.dataType} />
+        Your browser does not support the audio tag.
+      </audio>
+    {:else if file.dataType.startsWith("text/")}
+      <iframe src={file.objectUrl} class="w-full h-64" title="Text-File"
+      ></iframe>
     {:else}
       <slot />
     {/if}
