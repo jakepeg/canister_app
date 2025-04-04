ce4ec0b00a86cd3d715350b287d5ff5ccd30edf5
jakepeg <jakepeg@gmail.com>
2025-04-04 13:19:12 +0100

    format date + sorting

--- frontend/src/frontend/src/lib/shared/dates.ts
+++ frontend/src/frontend/src/lib/shared/dates.ts
@@ -11,15 +11,30 @@ export function formatUploadDate(uploaded_at: BigInt) {
   return uploadedAt.toLocaleTimeString("en-CH", dateOptions);
 }
 
+// export function formatUploadDateShort(uploaded_at: BigInt) {
+//   const dateOptions: Intl.DateTimeFormatOptions = {
+//     weekday: "short",
+//     year: "2-digit",
+//     month: "2-digit",
+//     day: "2-digit",
+//     timeZone: "CET",
+//     hour12: false,
+//   };
+//   let uploadedAt = new Date(Math.floor(Number(uploaded_at) / 1000000));
+//   return uploadedAt.toLocaleTimeString("en-CH", dateOptions);
+// }
+
 export function formatUploadDateShort(uploaded_at: BigInt) {
   const dateOptions: Intl.DateTimeFormatOptions = {
-    weekday: "short",
-    year: "2-digit",
-    month: "2-digit",
-    day: "2-digit",
-    timeZone: "CET",
-    hour12: false,
+    day: "numeric",    // 3
+    month: "long",     // April 
+    year: "numeric",   // 2025
+    hour: "2-digit",   // 18
+    minute: "2-digit", // 08
+    hour12: false,     // 24-hour format
+    timeZone: "CET"
   };
+  
   let uploadedAt = new Date(Math.floor(Number(uploaded_at) / 1000000));
-  return uploadedAt.toLocaleTimeString("en-CH", dateOptions);
+  return uploadedAt.toLocaleString("en-GB", dateOptions);
 }
